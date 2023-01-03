bcbd44bcdeafdd88fd03aac2845fddbfd7de2b1b
Gustavo Felski <gustavo_felski@hotmail.com>
2023-01-03 19:26:07 -0300

    development 0.03
    added Pages as test to controller
    added URL filter to define each controller will be redirect

--- app/libraries/core.php
+++ app/libraries/core.php
@@ -8,17 +8,39 @@
     */
 
     class Core {
-        protected $currentController = 'pages';
+        protected $currentController = 'Pages';
         protected $currentMethod = 'index';
         protected $param = [];
         
         public function __construct(){
-            $this->getURL();
-
+            //print_r($this->getURL());
+            $url = $this->getUrl();
+            if(empty($this->getURL())){
+                // fix url[0] empty 
+                // redirect to index.php
+                
+            }else{
+                // look in controllers for first value
+                if(file_exists('../app/controller/'.ucwords($url[0]).'.php')){
+                    // if exists, set as current controller
+                    $this->currentController = ucwords($url[0]);
+                    // unset [0] index
+                    unset($url[0]);
+                }
+            }
+            // require the controller
+            require_once '../app/controller/'.$this->currentController.'.php';
+            // instanciente controller class
+            $this->currentController = new $this->currentController;
         }
-
+        // function to get URL,Filter and add to array each word 
         public function getURL(){
-           echo $_GET['url'];
+           if(isset($_GET['url'])){ 
+                $url = rtrim($_GET['url'], '/'); // split url and '/'
+                $url = filter_var($url,FILTER_SANITIZE_URL); //clear url of any diferent character
+                $url = explode('/', $url);
+                return $url;
+           }
         }
     }
 
